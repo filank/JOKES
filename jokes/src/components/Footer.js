1f764e63350f959b88a78b419616fe3bbeaf0745
zuuuus <hamza.zahmoul@hotmail.com>
2022-08-12 19:06:42 +0100

    Footer component

--- jokes/src/components/Footer.js
+++ jokes/src/components/Footer.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer>
+      {' '}
+      <div className='backgroundFooterImage'>
+        <div>
+          <p>
+            GOT jOKES? GET PAID
+            <br /> FORSUBMITTING!
+          </p>
+          <div className='submitJoke'>
+            SUBMIT JOKE <i class='fa-solid fa-arrow-right-long'></i>{' '}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
